d85b50258498590a6a429969a0012eaae6fd8ac0
zqlimy17 <zqlimy@gmail.com>
2019-11-23 18:13:01 +0800

    updated new rating to reflect correct results on index page

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -70,7 +70,7 @@ reviews.put("/:id/new", async (req, res) => {
   let url = await `https://www.googleapis.com/books/v1/volumes/${req.params.id}`;
   console.log(url);
   await request(url, { json: true }, async (error, response, data) => {
-    let newRating = req.body.stars;
+    let newRating;
     Book.findOne({ id: req.params.id }, async (err, result) => {
       console.log(result);
       console.log(result.rating);
@@ -80,38 +80,38 @@ reviews.put("/:id/new", async (req, res) => {
           (parseFloat(req.body.stars) +
             parseFloat(result.rating) * parseFloat(result.ratingCount)) /
           (parseFloat(result.ratingCount) + 1);
+        Book.findOneAndUpdate(
+          {
+            id: req.params.id
+          },
+          {
+            id: data.id,
+            id: data.id,
+            title: data.volumeInfo.title,
+            description: data.volumeInfo.description,
+            img: data.volumeInfo.imageLinks.thumbnail,
+            author: data.volumeInfo.authors,
+            $set: { rating: newRating },
+            $inc: {
+              ratingCount: 1
+            }
+          },
+          {
+            upsert: true,
+            new: true
+          },
+          (err, book) => {
+            Review.create({
+              rating: req.body.stars,
+              review: req.body.review,
+              reviewer: req.session.currentUser._id,
+              book: book._id
+            });
+          }
+        );
       }
       console.log(newRating);
     });
-    Book.findOneAndUpdate(
-      {
-        id: req.params.id
-      },
-      {
-        id: data.id,
-        id: data.id,
-        title: data.volumeInfo.title,
-        description: data.volumeInfo.description,
-        img: data.volumeInfo.imageLinks.thumbnail,
-        author: data.volumeInfo.authors,
-        $set: { rating: newRating },
-        $inc: {
-          ratingCount: 1
-        }
-      },
-      {
-        upsert: true,
-        new: true
-      },
-      (err, book) => {
-        Review.create({
-          rating: req.body.stars,
-          review: req.body.review,
-          reviewer: req.session.currentUser._id,
-          book: book._id
-        });
-      }
-    );
   });
   res.redirect("/users/profile/" + req.session.currentUser.username);
 });
